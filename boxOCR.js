2953069353b12a43991926f798f4b9504391e182
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2020-09-24 17:55:17 -0300

    24/09/2020 as 17:55

--- boxOCR.js
+++ boxOCR.js
@@ -10,10 +10,22 @@ const worker = createWorker({
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
 
-    const { data: { text } } = await worker.recognize(imagem, {
+    const x = { data: { text } } = await worker.recognize(imagem, {
       rectangle: { top: 118, left: 624, width: 422, height: 18 },
-    });
-    console.log(text);
+    })
+
+    const y = { data: { text } } = await worker.recognize(imagem, {
+        rectangle: { top: 184, left: 130, width: 500, height: 18 },
+      })
+
+      const z = { data: { text } } = await worker.recognize(imagem, {
+        rectangle: { top: 184, left: 645, width: 215, height: 18 },
+      })
+      
+    console.log('13 - CÓDIGO DE BARRAS :',x.data.text);
+    console.log('01 - CÓDIGO/ESPECIFICAÇÃO DA RECEITA :',y.data.text);
+    console.log('02 - DATA VENCIMENTO :',z.data.text);
+
     await worker.terminate();
   })();
 
